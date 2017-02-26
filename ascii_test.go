bf2f8866bdca088a85a1f3de9b5f7811c69b9c65
uia-student <TestMail@example.com>
2017-02-26 16:21:12 +0100

    Ascii-Check

--- ascii_test.go
+++ ascii_test.go
@@ -1,43 +1,46 @@
 package main
 
 //import "testing"
-import "strings"
-import "./ascii"
+//import "strings"
+//import "./ascii"
 import "testing"
-//var ascii_tests_string = []struct {
-//	n1		 string
-//	expected string
-//}{
-//	{"\x22\x48\x65\x6C\x6C\x6F\x20\x3A\x2D\x29\x22"},
-//	{"\x22\x48\x65\x6C\x6C\x6F\x20\x3A\x2D\x29\x22"},
+import "fmt"
+func isASCII(s string) bool {
+	for _, c := range s {
+		if c > 127 {
+			return false
+		}
+	}
+	return true
+}
+
+func TestGreetingASCII(t *testing.T) {
+	if isASCII("a") == false {
+	fmt.Println("Success: Inneholder bare ASCII chars")
+	}else{
+	t.Error("Feil: Inneholder normal chars ")
+	}
+	}
+	
+
+
+//func TestGreetingsASCII(t *testing.T) {
+//    for_, v := range ascii_tests_string {
+ //   if !strings.Contains(Ascii, strings.ToLower(string(v))) {
+//	return false
+//	}
+ //    }
+ // return true
 //}
-//
-//func TestGreetingASCII(t *testing.T) {
-//	for _, v := range ascii_tests_string {
-//		if val := ascii.GreetingASCII(); val != v.expected {
-//			t.Errorf("String (%c) returned %c, expected %c", val, v.expected)
+
+
+//func TestFatal(t *testing.T) {
+//	str := "x22\x48\x65\x6C\x6C\x6F\x20\x3A\x2D\x29\x22"
+//	for i:= 0; i < len(str); i++ {
+//		for j := 0; i < len(ascii.Ascii); j++ {
+//			if str[i] != ascii.Ascii[j] {
+//				t.Error("expected", str[i], "got", ascii.Ascii[j])
+//			}
 //		}
 //	}
 //}
-
-
-func TestGreetingsASCII(t *testing.T) {
-    for_, v := range ascii_tests_string {
-    if !strings.Contains(Ascii, strings.ToLower(string(v))) {
-	return false
-	}
-     }
-  return true
-}
-
-
-func TestFatal(t *testing.T) {
-	str := "x22\x48\x65\x6C\x6C\x6F\x20\x3A\x2D\x29\x22"
-	for i:= 0; i < len(str); i++ {
-		for j := 0; i < len(ascii.Ascii); j++ {
-			if str[i] != ascii.Ascii[j] {
-				t.Error("expected", str[i], "got", ascii.Ascii[j])
-			}
-		}
-	}
-}
