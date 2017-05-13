c504faea135c69faaa29359659a61ac0fc5f3564
Sifo97 <sindrf16@student.uia.no>
2017-05-13 19:19:42 +0200

    Endringer på extended ascii

--- main_fileanal.go
+++ main_fileanal.go
@@ -5,17 +5,20 @@ import (
 )
 
 func main() {
-var fileToOpen = "files/lang01.wl"
-var fileToOpen2 = "files/lang02.wl"
-var fileToOpen3 = "files/lang03.wl"
-c := fileutils.FileToByteslice(fileToOpen)
-fmt.Println("01: ",c)
-c = fileutils.FileToByteslice(fileToOpen2)
-fmt.Println("------------------------------------------------------------------------------------------")
-fmt.Println("02: ",c)
-fmt.Println("------------------------------------------------------------------------------------------")
-c = fileutils.FileToByteslice(fileToOpen3)
-fmt.Println("03: ",c)
-fmt.Println("------------------------------------------------------------------------------------------")
-fmt.Println("Printed all 3 files.")
+	var fileToOpen = "files/lang01.wl"
+	var fileToOpen2 = "files/lang02.wl"
+	var fileToOpen3 = "files/lang03.wl"
+	
+	c := fileutils.FileToByteslice(fileToOpen)
+	fmt.Println("01: ",c)
+	
+	c = fileutils.FileToByteslice(fileToOpen2)
+	fmt.Println("------------------------------------------------------------------------------------------")
+	fmt.Println("02: ",c)
+	fmt.Println("------------------------------------------------------------------------------------------")
+	c = fileutils.FileToByteslice(fileToOpen3)
+	
+	fmt.Println("03: ",c)
+	fmt.Println("------------------------------------------------------------------------------------------")
+	fmt.Println("Printed all 3 files.")
 }
