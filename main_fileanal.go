805173b4dd78c06ce210e24ba4660454f7930f08
uia-student <TestMail@example.com>
2017-05-13 18:33:19 +0200

    Fix

--- main_fileanal.go
+++ main_fileanal.go
@@ -6,7 +6,16 @@ import (
 
 func main() {
 var fileToOpen = "files/lang01.wl"
-fmt.Println("Opening: ", fileToOpen)
+var fileToOpen2 = "files/lang02.wl"
+var fileToOpen3 = "files/lang03.wl"
 c := fileutils.FileToByteslice(fileToOpen)
-fmt.Println("method: ",c)
+fmt.Println("01: ",c)
+c = fileutils.FileToByteslice(fileToOpen2)
+fmt.Println("------------------------------------------------------------------------------------------")
+fmt.Println("02: ",c)
+fmt.Println("------------------------------------------------------------------------------------------")
+c = fileutils.FileToByteslice(fileToOpen3)
+fmt.Println("03: ",c)
+fmt.Println("------------------------------------------------------------------------------------------")
+fmt.Println("Printed all 3 files.")
 }
