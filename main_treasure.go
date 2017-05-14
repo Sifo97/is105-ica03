62d679f01bab54a7beeba89d971b2c9cfb551556
uia-student <TestMail@example.com>
2017-05-14 15:33:36 +0200

    Last

--- main_treasure.go
+++ main_treasure.go
@@ -2,17 +2,14 @@ package main
 import (
 	"fmt"
 	"./fileutils"
-	"./treasure"
+	//"./treasure"
 )
 
 func main() {
 	var fileToOpen = "treasure/treasure.txt"
-	
 	c := fileutils.FileToByteslice(fileToOpen)
-	s := string(c[:len(c)])
-	r := treasure.PrintTreasureUTF8(s)
-	fmt.Print(r)
-	for i := 0; i < len(r); i++ {
-		fmt.Printf("%c", r[i])
+	fmt.Printf("%c", c)
+	for i := 0; i < len(c); i++ {
+		fmt.Printf("%c", c[i])
 	}
 	}
