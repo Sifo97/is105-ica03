9b8d25f084b51ac1baa13f5b3ec64c6a01d6143d
Sifo97 <sindrf16@student.uia.no>
2017-05-14 17:25:08 +0200

    fikset å i server.go

--- server/server.go
+++ server/server.go
@@ -22,7 +22,7 @@ func foo(w http.ResponseWriter, r *http.Request) {
 
 	// Her skriver man data som er respons til brukeren som har skrevet
 	// http://localhost:3000 i sin nettleser
-	w.Write([]byte("<font color=\"green\">Hvordan g\xe5r det, <b>\u16a6</b> ?</font><br/>"))
+	w.Write([]byte("<font color=\"green\">Hvordan g\u00E5r det, <b>\u16a6</b> ?</font><br/>"))
 	w.Write([]byte("\u16a6 - Thurs<br/>"))
 	w.Write([]byte("\u23f0"))
 }
